ffc22289f9bd7cc3505238eae749f8d39a111c6f
adeerlulu <ayunina103@gmail.com>
2026-01-08 14:53:16 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,28 @@
+// 作品資料庫 - 
+const projectData = [
+    { 
+        title: "我的第一件作品", 
+        category: "Visual Design", 
+        image: "images/w02-cover.jpg", 
+        link: "project-01.html" 
+    },
+    { 
+        title: "品牌形象設計", 
+        category: "Brand Identity", 
+        image: "images/w02-cover.jpg", 
+        link: "project-02.html" 
+    },
+    { 
+        title: "編輯排版實作", 
+        category: "Editorial Design", 
+        image: "images/w02-cover.jpg", 
+        link: "project-03.html" 
+    },
+    { 
+        title: "編輯排版實作", 
+        category: "Editorial Design", 
+        image: "images/w02-cover.jpg", 
+        link: "project-04.html" 
+    },
+    // 往下繼續新增...
+];
