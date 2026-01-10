99e455eea3b9fb7116880054eaf49425ba3c8028
adeerlulu <ayunina103@gmail.com>
2026-01-10 13:46:18 +0800

    Update data.js

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ window.projectData = [
         image: "images/w02-cover.jpg", 
         description: "延續封面水彩概念，以綠葉、水、環保帶進整本文案設計",
         details: ["images/w02-d-01.jpg", "images/w02-d-02.jpg"], // 更多細節圖
-        link: "project-template.html?id=p01" // 改成連往樣板並帶參數
+        link: "works.html?id=p01" // 改成連往樣板並帶參數
     },
     { 
         id: "p02",
@@ -16,7 +16,7 @@ window.projectData = [
         image: "images/w02-cover.jpg", 
         description: "第二件作品的設計理念...",
         details: ["images/w02-detail1.jpg"],
-        link: "project-template.html?id=p02"
+        link: "works.html?id=p02"
     },
         { 
         id: "p03", // 給每個作品一個唯一 ID
@@ -25,7 +25,7 @@ window.projectData = [
         image: "images/w02-cover.jpg", 
         description: "延續封面水彩概念，以綠葉、水、環保帶進整本文案設計",
         details: ["images/w02-d-01.jpg", "images/w02-d-02.jpg"], // 更多細節圖
-        link: "project-template.html?id=p01" // 改成連往樣板並帶參數
+        link: "works.html?id=p01" // 改成連往樣板並帶參數
     },
         { 
         id: "p04", // 給每個作品一個唯一 ID
@@ -34,6 +34,6 @@ window.projectData = [
         image: "images/w02-cover.jpg", 
         description: "延續封面水彩概念，以綠葉、水、環保帶進整本文案設計",
         details: ["images/w02-d-01.jpg", "images/w02-d-02.jpg"], // 更多細節圖
-        link: "project-template.html?id=p01" // 改成連往樣板並帶參數
+        link: "works.html?id=p01" // 改成連往樣板並帶參數
     },
 ];
